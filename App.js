5e11a5493f79245a6d46c4d86f24c83808922264
SarveshHardas <86554291+SarveshHardas@users.noreply.github.com>
2022-03-11 21:31:09 +0530

    Create App.js

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import React, {Componenet} from 'react'
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DrawerNavigator from './navigation/drawNavigation';
+import {NavigationContainer} from '@react-navigation/native'
 
-export default class App extends Componenet {
+export default class App extends Component {
   render()
   {
     return (
           <NavigationContainer>
-            <DrawerNavigator />
+            <DrawerNavigator/>
           </NavigationContainer> 
     );
   }
 }
-
-
