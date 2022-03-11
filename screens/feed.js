cf48094ca1130fe25256f3bd003072dab784e60e
SarveshHardas <86554291+SarveshHardas@users.noreply.github.com>
2022-03-11 21:32:39 +0530

    Update feed.js

--- screens/feed.js
+++ screens/feed.js
@@ -1,66 +1,81 @@
 import React, { Component } from 'react';
-import {Text, View, StyleSheet, Image } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { RFValue } from 'react-native-responsive-fontsize';
 import PostCard from './postCard';
 
+let posts = require('./temp_posts.json');
+
 export default class Feed extends Component {
-    renderItem=()=>{
-        <PostCard/>
-    }
-     render()
-    { 
-        return (
-            <View style={styles.container}>
-                <SafeAreaView style={styles.androidSafeArea} /> 
-                    <View style={styles.appTitle}>
-                        <View style={styles.appIcon}>
-                            <Image source={require("../assets/logo.png")}
-                            style={styles.iconImage} ></Image>        
-                        </View>   
-                        <View style={styles.appTitleTextContainer}>
-                            <Text style={styles.appTitleText}>Spectagram</Text>
-                        </View>             
-                    </View> 
-                    <View style={styles.cardContainer}>
-                        <FlatList keyExtractor={this.keyExtractor}
-                        data={posts}
-                        renderItem={this.renderItem}/>
-                    </View>                       
-            </View> 
-        );
-    }
+  renderItem = () => {
+    <PostCard Post={posts} />;
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <SafeAreaView style={styles.androidSafeArea} />
+        <View style={styles.appTitle}>
+          <View style={styles.appIcon}>
+            <Image
+              source={require('../assets/logo.png')}
+              style={styles.iconImage}></Image>
+          </View>
+          <View style={styles.appTitleTextContainer}>
+            <Text style={styles.appTitleText}>Spectagram</Text>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <FlatList
+            keyExtractor={this.keyExtractor}
+            data={posts}
+            renderItem={this.renderItem}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:"black",
-    },
-    androidSafeArea:{
-        marginTop: Platform.OS === 'android' ? StatusBar.currentHeight:RFValue(35)
-    },
-    appTitle:{
-      flex:0.07,
-      flexDirection:'row',
-    },
-    appIcon:{
-      flex:0.2,
-      justifyContent:'center',
-      alignSelf:'center',
-    },
-    iconImage:{
-        width:'100%',
-        height:'100%',
-        resizeMode:'contain',
-    },
-    appTitleTextContainer:{
-        flex:0.8,
-        justifyContent:'center'
-    },
-    appTitleText:{
-        color:'white',
-        fontSize:RFValue(28)
-    },
-    cardContainer:{
-        flex:0.85
-    }
-  })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  androidSafeArea: {
+    marginTop:
+      Platform.OS === 'android' ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: 'row',
+  },
+  appIcon: {
+    flex: 0.2,
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  iconImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  appTitleTextContainer: {
+    flex: 0.8,
+    justifyContent: 'center',
+  },
+  appTitleText: {
+    color: 'white',
+    fontSize: RFValue(28),
+  },
+  cardContainer: {
+    flex: 0.85,
+  },
+});
